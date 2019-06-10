fa9440cecaae672c4598d8db720b824d296bc85f
Patton Nguyen <pattonnguyen@northwesternmutual.com>
2019-06-10 14:32:14 -0500

    add duplicate check commit

--- src/App.js
+++ src/App.js
@@ -8,15 +8,37 @@ class App extends React.Component {
     wishList: []
   }
 
+  checkinput = (event) => {
+    if (event === '') {
+      return false
+    } else {
+      for (let i = 0; i < this.state.availableList.length; i++) {
+        if (event === this.state.availableList[i]) {
+          return false
+        }
+      }
+      for (let i = 0; i < this.state.wishList.length; i++) {
+        if (event === this.state.wishList[i]) {
+          return false
+        }
+      }
+      return true
+    }
+  }
 
 
   render() {
     return (
       <div>
         <h1>My Wish List</h1>
-        <h3>Available Items</h3>
+
+        <h3>Available Items  {this.state.availableList.length}
+        </h3>
         <input placeholder='Type here' type="text" value={this.state.temp} onChange={event => (this.setState({ temp: event.target.value }))} />
-        <button onClick={() => { (this.state.temp !== '') ? (this.setState({ availableList: [...this.state.availableList, this.state.temp] }, this.setState({ temp: '' }))) : (console.log('error')) }}>Add to Available</button>
+        <button onClick={() => {
+          (this.checkinput(this.state.temp)) ?
+            (this.setState({ availableList: [...this.state.availableList, this.state.temp] }, this.setState({ temp: '' }))) : (console.log('error'))
+        }}>Add to Available</button>
         <ul>
           {this.state.availableList.map(item => {
             return (
@@ -32,13 +54,18 @@ class App extends React.Component {
             )
           })}
         </ul>
-        <h3>My Wish List</h3>
+        <h3>My Wish List {this.state.wishList.length}</h3>
         <ul>
           {this.state.wishList.map(item => {
             return (
               <div>
                 <li>{item}
-                  <button onClick={() => { this.setState({ availableList: [...this.state.availableList, item], wishList: this.state.wishList.filter(e => e !== item) }) }}>Remove from Wish List</button></li>
+                  <button onClick={() => {
+                    const index = this.state.wishList.indexOf(item)
+                    const array = this.state.wishList.slice();
+                    array.splice(index, 1)
+                    this.setState({ availableList: [...this.state.availableList, item], wishList: array })
+                  }}>Remove from Wish List</button></li>
               </div>
             )
           })}
